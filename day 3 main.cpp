957d6f470c1ad669e63db65dc877c8bce071d036
Techtalks26 <sonikirti567@gmail.com>
2026-05-02 14:06:45 +0530

    Update day 3 main.cpp

--- day 3 main.cpp
+++ day 3 main.cpp
@@ -23,3 +23,20 @@ int main () {
         cout << i << "";
     }
 }
+
+
+
+
+//program--3 
+//print 1 to 10 no using whilw loop 
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+   int i=1;
+   while(i <= 10){
+       cout<<i<<" ";
+       i++;
+    }
+}
