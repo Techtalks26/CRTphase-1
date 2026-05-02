394e907540003ec288a0db9c61932da2e6b003bf
Techtalks26 <sonikirti567@gmail.com>
2026-05-02 14:10:49 +0530

    Update day 3 main.cpp

--- day 3 main.cpp
+++ day 3 main.cpp
@@ -26,6 +26,23 @@ int main () {
 
 
 
+//program--4 
+//print odd no using whilw loop 
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+   int i=1;
+   while(i <= 10){
+       cout<<i<<" ";
+       i = i+2 ;
+    }
+     return 0;
+}
+
+
+
 
 //program--3 
 //print 1 to 10 no using whilw loop 
