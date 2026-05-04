153d5715ae4711a876f6da82684d89080c106898
Techtalks26 <sonikirti567@gmail.com>
2026-05-04 13:51:29 +0530

    Update day 4 main.cpp

--- day 4 main.cpp
+++ day 4 main.cpp
@@ -11,3 +11,19 @@ int main () {
         cout<<"\n";
     }
 }
+
+
+
+ //program-1
+//print no. 1 to 5 , 5 times
+#include <iostream>
+using namespace std;
+
+int main () {
+    for(int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            cout << "*";
+        }
+        cout<<"\n";
+    }
+}
