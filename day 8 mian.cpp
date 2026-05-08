fc3aa8179d635e01bbf73c318eb5139696e9a120
Techtalks26 <sonikirti567@gmail.com>
2026-05-08 14:03:38 +0530

    Update day 8 mian.cpp

--- day 8 mian.cpp
+++ day 8 mian.cpp
@@ -1,1 +1,64 @@
+#include<iostream>
+using namespace std;
 
+//problem -- 1 
+//types of inheritance 
+// 1.1 -- single level inheritance 
+
+class vehicle {
+    public:
+    void start() {
+        cout << "vehicle starts" << "\n";
+    }
+};
+
+class car : public vehicle {
+    public:
+    void drive(){
+        cout << "car is running" << "\n";
+    }
+};
+
+
+int main () {
+    car obj1;
+    obj1. drive();
+    obj1.start();
+}
+
+
+
+#include<iostream>
+using namespace std;
+
+//problem -- 1 
+//types of inheritance 
+// 1.2 -- multi level inheritance 
+
+class vehicle {
+    public:
+    void start() {
+        cout << "vehicle starts" << "\n";
+    }
+};
+
+class car : public vehicle {
+    public:
+    void drive(){
+        cout << "car is running" << "\n";
+    }
+};
+
+class speedometer : public car {
+    public:
+    void speed() {
+        cout << "car is running at 60 km/h" << "\n";
+    }
+};
+
+int main () {
+    speedometer obj1;
+    obj1.start();
+    obj1. drive();
+    obj1.speed();
+}
