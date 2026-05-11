1011a0ed5d24c5dd58ee4813ef2f75ede545dba5
Techtalks26 <sonikirti567@gmail.com>
2026-05-11 16:23:15 +0530

    Update day 11 main.cpp

--- day 11 main.cpp
+++ day 11 main.cpp
@@ -1,4 +1,4 @@
-//leetcode Q=189
+//problem -1 -- leetcode Q=189
 //rotate array
 
 class Solution {
@@ -21,3 +21,84 @@ void reverse(vector<int>& nums,int start, int end) {
       reverse(nums, k, n-1); // rotate rest of the elements   
     }
 };
+
+
+//problem - 2 --leetcode Q=344
+//reverse string 
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start = 0;
+        int end = s.size()-1;
+
+        while (start < end) {
+            swap(s[start] , s[end]);
+            start++;
+            end--;
+        }
+    }
+};
+
+
+//problem - 3 leetcode Q=796
+//rotate string 
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) {
+            return false;
+        }
+        string doublestring = s+s;
+        return doublestring.contains(goal);
+    }
+};
+
+//problem - 4 -- leetcodeQ=242
+//valid anagram 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        sort(s.begin(),s.end());
+        sort(t.begin() , t.end());
+        if (s == t) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+};
+
+
+
+//problrm - 5 
+//palindrome string 
+#include<iostream>
+using namespace std;
+
+bool ispalindrome (string str){
+    int start = 0;
+    int end = str.size() - 1;
+    while(start < end) {
+        if(str[start] != str[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+ int main() {
+     string str;
+     cout << "enter a string...";
+     cin >> str;
+     
+     if(ispalindrome(str)) {
+         cout << "given string is a palindrome string...";
+     }else {
+         cout << " given string is not a palindrome string...";
+     }
+     return 0;
+ }
