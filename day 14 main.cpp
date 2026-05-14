1d3a864e6152614482deb99939e176f942ac9615
Techtalks26 <sonikirti567@gmail.com>
2026-05-14 16:00:37 +0530

    Update day 14 main.cpp

--- day 14 main.cpp
+++ day 14 main.cpp
@@ -151,3 +151,168 @@ int main()
     return 0;
 }
 
+
+// Problem -- 4
+// delete the data fro the head and tail in L.L
+#include<iostream>
+using namespace std;
+class Node{
+public:    
+    int data;
+    Node* next;
+    
+    Node(int new_data) {
+        this->data = new_data;
+        this->next = nullptr;
+    }
+};
+
+Node* head = nullptr;
+
+void insertAtHead(int data) {
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    }
+
+void insertAtTail(int data) {
+    Node* newNode = new Node(data);
+    Node*temp = head;
+    while(temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insertAtPosition(int data, int position) {
+    Node* newNode = new Node(data);
+    Node* temp = head;
+    for(int i = 1; i < position - 1; i++) {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void deleteathead(){
+    head = head->next;
+}
+
+void deleteattail(){
+    Node* temp = head;
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = nullptr;
+}
+void display() {
+    Node* temp = head;
+    while(temp != nullptr) {
+        cout << temp->data <<" ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int main()
+{
+    head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next =new Node(40);
+    //insertAtHead(5);
+    //insertAtTail(50);
+    //insertAtPosition(25,3);
+    //deleteathead();
+    deleteattail();
+    display();
+    return 0;
+}
+
+
+
+
+// Problem -- 5
+// delete the data from specific position in LL 
+#include<iostream>
+using namespace std;
+class Node{
+public:    
+    int data;
+    Node* next;
+    
+    Node(int new_data) {
+        this->data = new_data;
+        this->next = nullptr;
+    }
+};
+
+Node* head = nullptr;
+
+void insertAtHead(int data) {
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    }
+
+void insertAtTail(int data) {
+    Node* newNode = new Node(data);
+    Node*temp = head;
+    while(temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void insertAtPosition(int data, int position) {
+    Node* newNode = new Node(data);
+    Node* temp = head;
+    for(int i = 1; i < position - 1; i++) {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void deleteathead(){
+    head = head->next;
+}
+
+void deleteattail(){
+    Node* temp = head;
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = nullptr;
+}
+void deleteatposition(int position) {
+    Node* temp = head;
+    for(int i =1; i < position - 1; i++) {
+        temp = temp->next;
+    }
+    temp->next = temp->next->next;
+}
+void display() {
+    Node* temp = head;
+    while(temp != nullptr) {
+        cout << temp->data <<" ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int main()
+{
+    head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next =new Node(40);
+    //insertAtHead(5);
+    //insertAtTail(50);
+    //insertAtPosition(25,3);
+    //deleteathead();
+    //deleteattail();
+    deleteatposition(3);
+    display();
+    return 0;
+}
