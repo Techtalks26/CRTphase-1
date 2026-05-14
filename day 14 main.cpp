eb797cabd5db9b1814fdb676f84891c0b5429932
Techtalks26 <sonikirti567@gmail.com>
2026-05-14 13:53:43 +0530

    Create day 14 main.cpp

---
+++ day 14 main.cpp
@@ -0,0 +1,42 @@
+//problem -- 1 
+//insert the data at the head of the linked list
+#include<iostream>
+using namespace std;
+
+class Node{
+ public:   
+    int data;
+    Node * next;
+    
+    Node (int new_data) {
+         this -> data = new_data;
+         this -> next = nullptr;
+    }
+};
+
+Node * head = nullptr;
+
+void insertathead(int data){
+    Node * newNode = new Node(data);
+    newNode -> next = head;
+    head = newNode;
+}
+void display() {
+    Node * temp = head;
+    while(temp != nullptr) {
+       cout << temp->data << " ";
+       temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int main(){
+    Node * head = new Node(10);
+    head -> next = new Node(20);
+    head->next->next= new Node(30);
+    head->next->next->next= new Node(40);
+    insertathead(5);
+    display();
+    return 0;
+}
+
