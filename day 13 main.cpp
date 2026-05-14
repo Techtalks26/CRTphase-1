ca1a2611f9a70ab6c887635b58e8b5cbfc3c4dc9
Techtalks26 <sonikirti567@gmail.com>
2026-05-14 16:46:39 +0530

    Update day 13 main.cpp

--- day 13 main.cpp
+++ day 13 main.cpp
@@ -1,3 +1,28 @@
+//Problem 1 LEET CODE 70
+
+class Solution {
+public:
+
+int stairsclimb(int n){
+    //base condition
+    if(n==0 || n==1){
+        return 1;
+    }
+    if (n==2){
+        return 2;
+    }
+//logic
+return stairsclimb(n-1) + stairsclimb(n-2);
+}   
+ int climbStairs(int n) {
+return stairsclimb(n);
+ }
+};
+
+
+
+//problem -- 2 
+//traversal linked list
 #include<iostream>
 using namespace std;
 
